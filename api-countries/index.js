5aed17531592eee9ba08b0e1b136039d5c6c818a
momin <faruk373@gmail.com>
2021-04-06 17:40:59 +0200

    backend

--- api-countries/index.js
+++ api-countries/index.js
@@ -9,7 +9,11 @@ app.use(cors())
 const port = 8000;
 
 app.get('/countries', (req, res) => {
-  res.send([" france  ",  " germany  ", " england  ",  " belgique ", "suisse", "italie", "japon"]);
+  res.send([" france  ",   " germany  ", " england  ",  " belgique ", "suisse", "italie", "mexico"]);
+
+});
+app.get('/countries/capital', (req, res) => {
+  res.send([" paris  ",   " berlin  ", " londres  ",  " bruxelle ", "bern", "rome", "mexico city"]);
 
 });
 
