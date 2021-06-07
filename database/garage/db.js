28129203ae172b0f0b74f5ac6b15f06f2e4522f5
faruk <faruk373@gmail.com>
2021-06-07 18:25:26 +0200

    base

--- database/garage/db.js
+++ database/garage/db.js
@@ -20,45 +20,65 @@ mongoose.connect("mongodb://localhost:27017/garage", (err) => {
 
     const car = mongoose.model("car", carSchema)
 
-    const deleteCar = async () =>{
-        const delMet = await car.deleteMany({marque:"Renault"},{marque:"Renault"})
+    const addCar = async () => {
+        const newCar = await car.insertMany([
+            {
+                marque: "Aston Martin",
+                model: "DB9",
+                year: 2010
+            },
+            {
+                marque: "Range Rover",
+                model: "Discovery Sport",
+                year: 2017
+            },
+            {
+                marque: "Toyota",
+                model: "209",
+                year: 1999
+            }
+        ])
     }
-    
+
+    const deleteCar = async () => {
+        const delMet = await car.deleteMany({ marque: "Renault" }, { marque: "Renault" })
+    }
+
 
     const updateYear = async () => {
-        const year = await car.update({_id:"60be18ad6177157f7b665232"}, {year:2000})
+        const year = await car.update({ _id: "60be18ad6177157f7b665232" }, { year: 2000 })
 
 
     }
 
-        // const id = "60be18ad6177157f7b665232";
-        // console.log("id",id)
-        
-        // car.findById(id, function (err,car) {
-            //     console.log("marque:", `${car.marque} ${car.model}`)
+    // const id = "60be18ad6177157f7b665232";
+    // console.log("id",id)
+
+    // car.findById(id, function (err,car) {
+    //     console.log("marque:", `${car.marque} ${car.model}`)
     // })
 
-    // const newCar = new car({
+    // const car1 = new car({
     //     marque: "Renault",
     //     model: "Espace",
     //     year: 1999
     // })
 
-    // const myCar = new car({
+    // const car2 = new car({
     //     marque: "Renault",
     //     model: "Scanic",
     //     year: 2004
 
     // })
-    // const herCar = new car({
+    // const car3 = new car({
     //     marque: "Peugeot",
     //     model: "308",
     //     year: 2017
 
     // })
-    
-    
-    deleteCar();
+
+
+    addCar();
 })
 
 
