61adf56b749d59045b0aba1499957849da4b987d
faruk <faruk373@gmail.com>
2021-06-09 12:31:03 +0200

    data

---
+++ databaseHero/hero/hero1.js
@@ -0,0 +1,36 @@
+// const express = require('express')
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+
+
+// mongoose.connect('mongodb://localhost:27017/heros',(err) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("I am connected to database");
+//     }
+// })
+
+
+// const heroSchema = mongoose.Schema({
+//     name: String,
+//     age: Number,
+//     date: { type: Date, default: Date.now }
+// })
+
+// const hero = mongoose.model("hero", heroSchema)
+
+// const port = 8000
+
+// const app = express()
+
+// app.use(cors())
+// app.use(express.json())
+
+
+
+
+
+// app.listen(port, () => {
+//     console.log(`J'écoute des requêtes sur le port ${port}`);
+// })
---
+++ databaseHero/hero/model/hero.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+// Import mongoose
+
+
+ //Creation de schema 
+ // Creer les cles  de schema et definir le type leur valeur  
+const heroSchema = new mongoose.Schema({
+    name: String,
+    powers:[String],
+    color: String,
+    isAlive: Boolean,
+    age: Number,
+    image: String,
+    created: { type: Date, default: Date.now }
+})
+
+// Creation de model
+const Hero = mongoose.model('Hero', heroSchema)
+
+// Expoter le module
+module.exports = Hero
