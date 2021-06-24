4b574ca4e7790f10dbfeae90f6f8a2bd1315cad9
faruk <faruk373@gmail.com>
2021-06-25 00:09:34 +0200

    commit

--- express_login/server.js
+++ express_login/server.js
@@ -2,10 +2,10 @@ const express = require("express")
 const mongoose = require("mongoose")
 const userModel = require("./models/user")
 const validation = require("./validation")
-// const bcryptjs = require("bcryptjs")
-// const jwt = require("jsonwebtoken")
+const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const config = require("./config.js")
-const {validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 const validate = require("./validation")
 
 
@@ -23,8 +23,7 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/signup',validate, async (req, res) => {
-
+app.post('/signup', validate, async (req, res) => {
 
     try {
         const errors = validationResult(req);
@@ -35,21 +34,85 @@ app.post('/signup',validate, async (req, res) => {
             })
         } else {
             const userInfo = req.body
-            userInfo.date = new Date(userInfo.date);
+            const password = bcryptjs.hashSync(req.body.password)
+            userInfo["password"] = password
+            console.log("userInfo got", userInfo)
 
+            userInfo.date = new Date(userInfo.date);
             const newUser = await userModel.create(userInfo)
             console.log("new date", newUser)
             res.json({
                 "mesage": "user created", newUser
             })
         }
-
     } catch (err) {
         console.log(err, "can't sign up")
     }
 })
 
 
+
+app.post('/login', async (req, res) => {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+    // console.log("password",userPassword)
+    // console.log("userEmail",userEmail)
+
+    try {
+        // console.log("givenPasswoed")
+        const givenPassword = bcryptjs.hashSync(userPassword)
+        // console.log("givenPasswoed", givenPassword)
+
+        const userDetail = await userModel.find({ email: userEmail })
+        // console.log("userDetail", userDetail)
+
+        const isValidUser = bcryptjs.compareSync(userPassword, givenPassword)
+        // console.log("isValidUser",isValidUser)
+
+
+        if (isValidUser) {
+            const validToken = await jwt.sign({
+                id: userDetail.id
+            }, "secret", {
+                expiresIn: "300s"
+
+            })
+            // console.log("validToken", validToken)
+
+            res.json({ mesage: `${userEmail} is  logged in`, validToken })
+        } else {
+            res.json({ message: `${userEmail} is not identified user` })
+        }
+
+    } catch (error) {
+        res.json({
+            message: "something went wrong"
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log("The server is waiting for requests")
 })
+
+
+
+
+// try {
+//     const foundPasswords = bcrypt.compareSync(logPass,password)
+//     console.log("foundPasswords",foundPasswords)
+//     if (foundPasswords) {
+//         const token = jwt.sign(
+//             {
+//                 id: user._id
+//             }, config.secret,
+//             {
+//                 expiresIn: 60 * 60
+//             })
+//         res.json({ message: "you are now login" })
+//     } 
+// }
+
+// catch (error) {
+//     res.status(500).json({ message: "There was an error while treating the request" })
+// }
